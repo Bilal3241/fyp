b50ce7214ba6961db6325254da6f4323b7b7e2ac
anzaladar <anzaladar598@gmail.com>
2020-12-03 00:03:44 +0500

    home

--- LCT/App.js
+++ LCT/App.js
@@ -1,34 +1,23 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import Home from './screens/Home';
+import Splash from './screens/Splash';
 import Post_add from './screens/Post_add';
 
-const App: () => React$Node = () => {
-  return (
-    <Post_add/>
-  );
-};
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import {Header,Container,Left,Icon} from 'native-base';
+const Drawer = createDrawerNavigator();
 
-export default App;
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Splash">
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name="Splash" component={Splash} />
+        <Drawer.Screen name="Post Add" component={Post_add} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+   
+    
+  );
+}
