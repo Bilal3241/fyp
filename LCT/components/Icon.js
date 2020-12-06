1098ca503d7378018d437894a080c3fce55cd0c3
MahamMubashir <mahammubashir976@gmail.com>
2020-12-06 15:11:43 +0500

    Icon.js

---
+++ LCT/components/Icon.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { TouchableOpacity,View ,Image,StyleSheet} from 'react-native';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
+function Icon(props) {
+    return (
+        
+    <TouchableOpacity  onPress={()=>{alert("you clicked facebook")}}>
+       <Image style={styles.button} 
+       source={props.imageName}/>
+       </TouchableOpacity>
+    );
+}
+const styles = StyleSheet.create({
+    button:{
+        height:hp('15%'),
+        width:wp('30%'),
+        borderRadius:hp('20%'),
+        marginTop:hp('63%'),
+        margin:hp('5%'),
+    },
+})
+export default Icon;
