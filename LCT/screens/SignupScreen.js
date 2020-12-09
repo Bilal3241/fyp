f2cbfee7f0dc6c3a363977b54a5e5511eff20341
MahamMubashir <mahammubashir976@gmail.com>
2020-12-09 12:09:17 +0500

    fixing a problem

--- LCT/screens/SignupScreen.js
+++ LCT/screens/SignupScreen.js
@@ -3,31 +3,21 @@ import { ImageBackground ,StyleSheet,View,TouchableOpacity,Text,Image} from 'rea
 import Icon from  '../components/Icon';
 
 import {widthPercentageToDP as wp , heightPercentageToDP as hp } from 'react-native-responsive-screen';
-import Logo from '../components/Logo';
-import colors from '../config/colors';
 function SignupScreen(props) {
     return (
     <ImageBackground
         source={require("../assets/bg.jpg")}
         style={styles.background}>
         <View style={styles.container}>
-
-
-            <Image style={styles.logo}source={require("../assets/LogoLTG.png")}></Image> 
-
-        <Logo ctop='15'/>
+        <Image style={styles.logo}source={require("../assets/LogoLTG.png")}></Image> 
         
         <Text style={styles.text}>
         Sign-up using facebook or gmail
         </Text>   
-
         
-            <Text style={styles.text}>
-                Sign-up using your gmail
-            </Text>   
+        <Icon  imageName={require("../assets/fb1.png")}/>
+        <Icon  imageName={require("../assets/gmail3.jpg")}/>
         
-            <Icon  imageName={require("../assets/gmail.png")}/>
-       
         </View>
 
     </ImageBackground>
@@ -37,35 +27,34 @@ const styles = StyleSheet.create({
     background:{
         flex:1,
         resizeMode: "cover",
+      
     },
     container:{
         flex:1,    
         flexDirection:'row',
         alignItems:'center',
         justifyContent:'center',
-
-        backgroundColor: 'rgba(0, 0, 0, 0.5)' 
+        backgroundColor: 'rgba(0, 0, 0, 0.5)'
+       
     },
     logo:{
         width:wp('80%'),
         height:hp('15%'),
         position:'absolute',
         top:hp('15%'),
+        
     },
-
     text:{
-        color:colors.white,
+        color:'#ffffff',
         position:'absolute',
         height: hp('15%'), 
         width:wp('75%'),
         top:hp('60%'),
-
-        left:wp('25%'),
-
-        left:wp('20%'),
-
+        left:wp('15%'),
         fontSize:16,
         fontWeight:'bold'
+       
+
     }
 })
 
