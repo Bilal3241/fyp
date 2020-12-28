651a406ed7d688519cf3cb97d90d7ee2cbb4ed6c
MahamMubashir <mahammubashir976@gmail.com>
2020-12-28 19:10:43 +0500

    post reviews

---
+++ LCT/controller/AdsController/PostReview.js
@@ -0,0 +1,20 @@
+import Firestore from '@react-native-firebase/firestore';
+
+function PostReview(data) {
+    return (
+        Firestore().collection('Reviews').doc().set({
+            name: data.name,
+            profile: data.photo,
+            email: data.email,
+            item:data.item,
+            comment:data.des,
+        }).then(function() {
+            console.log("Document successfully written!");
+        })
+        .catch(function(error) {
+            console.log("Error writing document: ", error);
+        })
+    );
+}
+
+export default PostReview;
