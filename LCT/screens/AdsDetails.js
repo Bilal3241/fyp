93d8ae86232d6a5c853d8ea237254e801fd09b33
MahamMubashir <mahammubashir976@gmail.com>
2021-01-03 20:41:24 +0500

    Modal added Reserve Room

--- LCT/screens/AdsDetails.js
+++ LCT/screens/AdsDetails.js
@@ -10,6 +10,8 @@ import InputField from '../components/InputField';
 import Firestore, { firebase } from '@react-native-firebase/firestore';
 import Icon from 'react-native-vector-icons/Ionicons';  
 import PostReview from '../controller/AdsController/PostReview';
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+import { TextInput } from 'react-native-gesture-handler';
 function AdsDetails({route, navigation}) {
     var user=firebase.auth().currentUser;
 
@@ -29,13 +31,40 @@ function AdsDetails({route, navigation}) {
        
     }
 
-    function onpress() {
+    function onpress() {                                                                                                                                                                        
         setModalOpen(false);
     }
 
     function redirectGoogleMaps(){
     }
+
+    const [checkIn, setCheckIn] = useState(new Date('2020-12-20'));
+    const [checkOut, setCheckOut] = useState(new Date('2020-12-21'));
+    const [rooms, setRooms] = useState('');
+    const [fare, setFare] = useState(0);
+    const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+    console.log(checkIn);
+    const showDatePicker = () => {
+        setDatePickerVisibility(true);
+      };
+      const hideDatePicker = () => {
+        setDatePickerVisibility(false);
+      };
+      const handleInConfirm = (date) => {
+        setCheckIn(new Date(date.toDateString()));
+        console.warn("Check In has been picked: ", date.toDateString());
+        hideDatePicker();
+      };
+      const handleOutConfirm = (date) => {
+        setCheckOut(new Date(date.toDateString()))
+        console.warn("Check Out has been picked: ", date.toDateString());
+        setFare((checkOut.getTime()-checkIn.getTime())/86400000);
+        hideDatePicker();
+      };
+      var noOfDays=(checkOut.getTime()-checkIn.getTime())/86400000;
+
     const [modalOpen, setModalOpen] = useState(false);
+    const [modalOpen2, setModalOpen2] = useState(false);
     return (
         
         <ScrollView style={styles.background}> 
@@ -58,7 +87,56 @@ function AdsDetails({route, navigation}) {
             {/* <AppButton  title="Post" onPress={()=> {Reviews();onpress();setDesc("")} }></AppButton> */}
             </View>
         </Modal>
+           {/* NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN */}
+           <Modal
+           animationType="slide"
+           transparent={true}
+            visible={modalOpen2}>
+         
+            <View style={styles.modal2}>
+            <Icon name="close-outline" style={styles.icon} style onPress={()=> setModalOpen2(false)} size={50} color="black"></Icon>
            
+            <TouchableHighlight
+              style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
+              onPress={showDatePicker}>
+              <Text style={styles.textStyle}>Check In</Text>
+            </TouchableHighlight>
+                <DateTimePickerModal
+                  isVisible={isDatePickerVisible}
+                  mode='date'
+                  onConfirm={handleInConfirm}
+                  onCancel={hideDatePicker}
+                />
+                
+                
+                <TouchableHighlight
+              style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
+              onPress={showDatePicker}>
+              <Text style={styles.textStyle}>Check Out</Text>
+            </TouchableHighlight>
+                <DateTimePickerModal
+                  isVisible={isDatePickerVisible}
+                  mode='date'
+                  onConfirm={handleOutConfirm}
+                  onCancel={hideDatePicker}
+                />
+                
+                
+                
+                <TextInput placeholder= 'No. of Rooms' onChangeText={setRooms} defaultValue={rooms}></TextInput>
+                <Text style={styles.heading}>{fare}</Text>
+               
+                <TouchableHighlight
+              style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
+           >
+              <Text style={styles.textStyle}>Proceed to Payment</Text>
+            </TouchableHighlight>
+            </View>
+        </Modal>
+
+
+
+            {/* NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN */}     
             <Text style={styles.text}>
                 {route.params.apartment.Title}
             </Text>
@@ -81,10 +159,11 @@ function AdsDetails({route, navigation}) {
                  Charges: {route.params.apartment.Charges}  No of Rooms: {route.params.apartment.NoOfRooms}
             </Text>
             <View style={styles.btn}>
-            <AppButton  title="Reserve Room" press={()=>alert("Reserve Room")}></AppButton>
+            <AppButton  title="Reserve Room" onPress={()=> setModalOpen2(true)}></AppButton>
             <AppButton title="Start Chat" press={()=>alert("Start Chat")}></AppButton>
             <AppButton  title="Get Direction" onPress={() => Linking.openURL('google.navigation:q=100+101')}></AppButton>
             <AppButton  title="Post a Review" onPress={()=> setModalOpen(true)}></AppButton>
+            <AppButton  title="Make Payment" onPress={()=>navigation.navigate('StripePayment')}></AppButton>
             </View>
         </ScrollView>
         
@@ -158,6 +237,17 @@ const styles = StyleSheet.create({
             alignItems: 'center',
             shadowColor: "#000",
     },
+    modal2:{
+        width:wp('80%'),
+        height:hp('70%'),
+        marginTop:hp('15%') ,
+        marginLeft:wp('10%'),
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: 'center',
+        shadowColor: "#000",
+},
     openButton: {
         marginTop:hp("5%"),
         backgroundColor: "#F194FF",
