76a891f14ff4d4bf73e1aece55d227a7e842761f
anzaladar <anzaladar598@gmail.com>
2021-01-04 23:57:39 +0500

    chat nav added

--- LCT/components/Message.js
+++ LCT/components/Message.js
@@ -1,18 +1,20 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,TouchableOpacity  } from 'react-native'
 import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen';
 
-export default function Message(username) {
+export default function Message({username,nav,apart}) {
     return (
-        <View style={styles.item}>
-            <Text>username</Text>
-        </View>
+        
+        <TouchableOpacity style={styles.item} onPress={()=>nav.navigate('Chat',{apart})}>
+            <Text>{username}</Text>
+        </TouchableOpacity>
+        
     )
 }
 
 const styles = StyleSheet.create({
     item:{
-        height:hp('20%'),
+        height:hp('7%'),
         borderBottomColor:'black',
         borderBottomWidth:1,
     }
--- LCT/screens/MessageList.js
+++ LCT/screens/MessageList.js
@@ -1,39 +1,41 @@
 import React, {useState, useEffect} from 'react'
-import { StyleSheet, Text, View,ScrollView } from 'react-native';
+import { StyleSheet, Text, View,ScrollView,SafeAreaView } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 import Message from '../components/Message';
 import Firestore,{firebase} from '@react-native-firebase/firestore';
+import { heightPercentageToDP } from 'react-native-responsive-screen';
+import colors from '../config/colors';
 
-export default function MessageList({route,navigaton}) {
-    // const collectionList= [];
-    // var docref= firebase.firestore().collection('Chat').doc(route.params.apart.Owner);
-    // docref.get().then(function (doc){
-    //     if (doc.exists) {
-    //         console.log(doc.data())
-    //     }
-    // })
-    var msgList=[];
+function MessageList({route,navigation}) {
+    const apart=route.params.apart;
+    const [msgList,setMsgList]=useState([]);
     useEffect(() => {
-        
         const listref= Firestore().collection('Chat').doc(route.params.apart.Owner);
         listref.get().then(function(doc){
             if (doc.exists) {
-                msgList=doc.data().collections;
+                setMsgList(doc.data().collections);
             }
         })
     }, [])
 
     return (
-        
+        <SafeAreaView style={styles.container}>
             <FlatList  
-                keyExtractor={(customer) => customer.collection}
+                keyExtractor={(item) => item.collection}
                 data={msgList}
-                renderItem={({customer}) =>(
-                    <Message username={customer.customerName} />
-                )}
-            />
+                renderItem={({item}) =>(
+                     <Message username={item.customerName}   nav={navigation} apart={apart}  />
+                )}/>
+            </SafeAreaView>
         
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+       // marginTop: heightPercentageToDP("10%"),
+      },
+})
+
+export default MessageList;
