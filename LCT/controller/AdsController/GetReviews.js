77177180aa51d72466ebf9a4f6319bf13f8474ca
Bilal3241 <47459320+Bilal3241@users.noreply.github.com>
2021-01-19 21:18:58 +0500

    owner cannot see add review

--- LCT/controller/AdsController/GetReviews.js
+++ LCT/controller/AdsController/GetReviews.js
@@ -1,10 +1,11 @@
 import Firestore, { firebase } from '@react-native-firebase/firestore';
 import React, { useEffect, useState } from 'react';
 export async function getReviews(apartmentId, reviewsRetrevived) {
-const reviewList=[];
+console.log(apartmentId);
+   const reviewList=[];
 var review;
 var snapshot = await firebase.firestore()
-.collection('Reviews').where('item', '==', apartmentId.apartmentId).get()
+.collection('Reviews').where('item', '==', apartmentId).get()
 
  snapshot.forEach((doc)=>{
     review={id: doc.id, data:doc.data()};
--- LCT/screens/AdsDetals/AdsDetails.js
+++ LCT/screens/AdsDetals/AdsDetails.js
@@ -13,119 +13,95 @@ import ReviewsView from './ReviewsView';
 import DescriptionView from './DescriptionView';
 import { onChange } from 'react-native-reanimated';
 import colors from '../../config/colors';
-
 function AdsDetails({route, navigation}) {
-    var user=firebase.auth().currentUser;
-
-    const apart=route.params.apartment;
-    var user=firebase.auth().currentUser;
-    function setChatNav(){
-        if(user.email==route.params.apartment.Owner){
-            navigation.navigate('Messages',{apart})
-        }
-        else{
-            navigation.navigate('Chat',{apart})
-        }
+  var user=firebase.auth().currentUser;
+  const apart=route.params.apartment;
+  var user=firebase.auth().currentUser;
+  function setChatNav(){
+    if(user.email==route.params.apartment.Owner){
+      navigation.navigate('Messages',{apart})
     }
-
-    const [tabIndex, setTabIndex]=useState(0);
-
-    const [checkIn, setCheckIn] = useState(new Date());
-    const [checkOut, setCheckOut] = useState(new Date());
-    const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
-    const [flag, setFlag] = useState(false);
-    
-    const showDatePicker = () => {
-        setDatePickerVisibility(true);
-      };
-      const hideDatePicker = () => {
-        setDatePickerVisibility(false);
-      };
-      
-      
-      const handleConfirm = (date) => {
-        console.log(date.toDateString());
-        if(flag == false){
-          setCheckIn(new Date(date));
-        hideDatePicker();
-        setFlag(true);
-        }
-        if((flag == true)){
-        setCheckOut(new Date(date));
-        hideDatePicker();
-        setFlag(false);
-        }
-      };
-      const doReservation=()=>{
-         var NoOfDays=(checkOut.getTime()-checkIn.getTime())/86400000;
-         var price=NoOfDays*route.params.apartment.Charges;
-        var reservationData={apartmentDetails: route.params.apartment, totalFare: price, daysOfStay: NoOfDays, inDate: checkIn.toDateString(), outDate: checkOut.toDateString()} //checkIn, outDate: checkOut }
-        setReservationModal(false);
-        navigation.navigate("StripePayment", {reservationData});
-      };
-    const [ReservationModal, setReservationModal] = useState(false);
-    return (
-        
-        <ScrollView style={styles.background}> 
-           <Modal
-           animationType="slide"
-           transparent={true}
-            visible={ReservationModal}>
-         
-            <View style={styles.modal2}>
-            <Icon name="close-outline" style={styles.icon} style onPress={()=> setReservationModal(false)} size={50} color="black"></Icon>
-           
-            <TouchableOpacity
-              style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
-              onPress={showDatePicker}>
-              <Text style={styles.textStyle}>Check In</Text>
-              <DateTimePickerModal
-                  isVisible={isDatePickerVisible}
-                  mode='date'
-                  onConfirm={handleConfirm}
-                  onCancel={hideDatePicker}
-                  minimumDate={new Date()}
-                />
-            </TouchableOpacity>
-                
-                
-                
-                <TouchableOpacity
-              style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
-              onPress={showDatePicker}>
-              <Text style={styles.textStyle}>Check Out</Text>
-              <DateTimePickerModal
-                  isVisible={isDatePickerVisible}
-                  mode='date'
-                  onConfirm={handleConfirm}
-                  onCancel={hideDatePicker}
-                  minimumDate={new Date(checkIn)}
+    else{
+      navigation.navigate('Chat',{apart})
+    }
+  }
+  const [tabIndex, setTabIndex]=useState(0);
+  const [checkIn, setCheckIn] = useState(new Date());
+  const [checkOut, setCheckOut] = useState(new Date());
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+  const [flag, setFlag] = useState(false);
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };   
+  const handleConfirm = (date) => {
+    console.log(date.toDateString());
+    if(flag == false){
+      setCheckIn(new Date(date));
+    hideDatePicker();
+    setFlag(true);
+    }
+    if((flag == true)){
+    setCheckOut(new Date(date));
+    hideDatePicker();
+    setFlag(false);
+    }
+  };
+  const doReservation=()=>{
+    var NoOfDays=(checkOut.getTime()-checkIn.getTime())/86400000;
+    var price=NoOfDays*route.params.apartment.Charges;
+    var reservationData={apartmentDetails: route.params.apartment, totalFare: price, daysOfStay: NoOfDays, inDate: checkIn.toDateString(), outDate: checkOut.toDateString()} //checkIn, outDate: checkOut }
+    setReservationModal(false);
+    navigation.navigate("StripePayment", {reservationData});
+  };
+  const [ReservationModal, setReservationModal] = useState(false);
+  return (  
+    <View style={styles.background}> 
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={ReservationModal}>
+        <View style={styles.modal2}>
+          <Icon name="close-outline" style={styles.icon} style onPress={()=> setReservationModal(false)} size={50} color="black"></Icon>
+          <TouchableOpacity
+            style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
+            onPress={showDatePicker}>
+            <Text style={styles.textStyle}>Check In</Text>
+            <DateTimePickerModal
+              isVisible={isDatePickerVisible}
+              mode='date'
+              onConfirm={handleConfirm}
+              onCancel={hideDatePicker}
+              minimumDate={new Date()}
             />
-            </TouchableOpacity>
-                
-                                
-                <Text>No. of Rooms: {route.params.apartment.NoOfRooms}</Text>
-                <Text>chekIn: {checkIn.toDateString()}</Text>
-                <Text>checkOut: {checkOut.toDateString()}</Text>
-                {/*<Text>Days of Stay: {}</Text>
-                <Text>Fare: {Fare} {}</Text>*/}
-               
-                
-                <TouchableHighlight
-              style={{ ...styles.openButton, backgroundColor: "#2196F3" }}>
-              
-              <Text style={styles.textStyle} onPress={doReservation}>Proceed to Payment</Text>
-                </TouchableHighlight>
-            </View>
-        </Modal>
-
-        <View>
-          
-        <PicSlider style={styles.picslider}>
-
-        </PicSlider>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
+            onPress={showDatePicker}>
+            <Text style={styles.textStyle}>Check Out</Text>
+            <DateTimePickerModal
+              isVisible={isDatePickerVisible}
+              mode='date'
+              onConfirm={handleConfirm}
+              onCancel={hideDatePicker}
+              minimumDate={new Date(checkIn)}
+            />
+          </TouchableOpacity>                
+          <Text>No. of Rooms: {route.params.apartment.NoOfRooms}</Text>
+          <Text>chekIn: {checkIn.toDateString()}</Text>
+          <Text>checkOut: {checkOut.toDateString()}</Text>
+          <TouchableHighlight
+            style={{ ...styles.openButton, backgroundColor: "#2196F3" }}>
+            <Text style={styles.textStyle} onPress={doReservation}>Proceed to Payment</Text>
+          </TouchableHighlight>
         </View>
-        <View>
+      </Modal>
+      <View>
+        <PicSlider style={styles.picslider}></PicSlider>
+      </View>
+      <View>
         <SegmentedControlTab
           values={["Details", "Reviews"]}
           selectedIndex={tabIndex}
@@ -137,84 +113,84 @@ function AdsDetails({route, navigation}) {
           activeTabTextStyle={styles.activeTabTextStyel}
         />
         {tabIndex === 0 ? (
-        <DescriptionView apartment={route.params.apartment}/>
-      ) : (
-        <ReviewsView apartmentId={route.params.apartment.Location}/>
-      )}
-        </View>
-        <View style={styles.btn}>
-            <AppButton title="Start Chat" width='45' onPress={setChatNav}></AppButton>
-            <AppButton  title="Reserve Room" width='45' onPress={()=> setReservationModal(true)}></AppButton>
-            </View>
-        </ScrollView>
-        
-      
+          <DescriptionView apartment={route.params.apartment}/>
+        ) : (
+          <ReviewsView apartmentId={route.params.apartment} />
+        )}
+      </View>
+      <View style={styles.btn}>
+        <AppButton title="Start Chat" width='45' onPress={setChatNav}></AppButton>
+        {user.email!=route.params.apartment.Owner ?
+          <AppButton  title="Reserve Room" width='45' onPress={()=> setReservationModal(true)}></AppButton>
+          :<View/>
+        }
+      </View>
+    </View>
     );
 }
 const styles = StyleSheet.create({
-    background:{
-        flex:1,
-        backgroundColor:colors.white,
-    },
-    segmentTabStyle: {
-      backgroundColor: 'transparent',
-      borderColor: 'transparent',
-      borderRadius: 0,
-      borderLeftWidth: 0,
-      borderRightWidth: 0,
-    },
-    activeSegtmentTabStyle: {
-      borderBottomColor: colors.btnBlue,
-      borderBottomWidth: 2,
-      backgroundColor: 'transparent',
-    },
-    tabsContainerStyle: {
-      width: '100%',
-      height: hp('6%'),
-      borderRadius: 0,
-    },
-    tabTextStyle: {
-      fontWeight:'bold',
-      fontSize: hp('3%'),
-      paddingVertical:'3%',
-      color: colors.btnBlue,
-    },
-    activeTabTextStyel: {
-      fontWeight:'bold',
-      fontSize: hp('4%'),
-      color: colors.btnBlue,
-      opacity: 1,
-    },
-    
-    modal2:{
-        width:wp('80%'),
-        height:hp('70%'),
-        marginTop:hp('15%') ,
-        marginLeft:wp('10%'),
-        backgroundColor: 'rgba(190,190,190,0.9)',
-        borderRadius: 20,
-        padding: 35,
-        alignItems: 'center',
-        shadowColor: "#000",
-},
-btn:{
-  flex:1,
-  flexDirection:'row',
-  justifyContent:"center",
-  marginTop:'3%',
-},
-    openButton: {
-        marginTop:hp("5%"),
-        backgroundColor: "#F194FF",
-        borderRadius: 20,
-        padding: 10,
-        elevation: 2
-      },
-      textStyle: {
-        color: "white",
-        fontWeight: "bold",
-        textAlign: "center"
-      }
+  background:{
+    flex:1,
+    backgroundColor:colors.white,
+  },
+  segmentTabStyle: {
+    backgroundColor: 'transparent',
+    borderColor: 'transparent',
+    borderRadius: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+  },
+  activeSegtmentTabStyle: {
+    borderBottomColor: colors.btnBlue,
+    borderBottomWidth: 2,
+    backgroundColor: 'transparent',
+  },
+  tabsContainerStyle: {
+    width: '100%',
+    height: hp('6%'),
+    borderRadius: 0,
+  },
+  tabTextStyle: {
+    fontWeight:'bold',
+    fontSize: hp('3%'),
+    paddingVertical:'3%',
+    color: colors.btnBlue,
+  },
+  activeTabTextStyel: {
+    fontWeight:'bold',
+    fontSize: hp('4%'),
+    color: colors.btnBlue,
+    opacity: 1,
+  },
+  modal2:{
+    width:wp('80%'),
+    height:hp('70%'),
+    marginTop:hp('15%') ,
+    marginLeft:wp('10%'),
+    backgroundColor: 'rgba(190,190,190,0.9)',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: "#000",
+  },
+  btn:{
+    flex:1,
+    flexDirection:'row',
+    justifyContent:"center",
+    marginTop:'3%',
+  },
+  openButton: {
+    marginTop:hp("5%"),
+    backgroundColor: "#F194FF",
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  }
 })
 
 export default AdsDetails;
